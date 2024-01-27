df3c28f034febb4f767d8fe7eb8d89d269f5d69f
Konstantina Panagopoulou <151877044+konstantina44panag@users.noreply.github.com>
2024-01-27 19:21:14 +0200

    Update variables.py

--- variables.py
+++ variables.py
@@ -7,8 +7,7 @@ import subprocess
 subprocess.run(["python3", "preparation.py"])
 from preparation import df, Ask, Bid
 
-# 1,2,3
-# Find the last trade price, volume, and timestamp
+#1,2,3. The last trade/bid/ask price, volume, and timestamp
 trades_1min = df.resample("1T", on="time").agg(
     {"price": "last", "vol": "last", "time": "last"}
 )
@@ -56,7 +55,7 @@ print(trades_1min)
 print(asks_1min)
 print(bids_1min)
 
-# 4
+#4.Last price at which an aggressive buy/sell order for $10,000 would execute
 df["value"] = df["price"] * df["vol"]
 Ask["value"] = Ask["price"] * Ask["vol"]
 Bid["value"] = Bid["price"] * Bid["vol"]
@@ -81,13 +80,12 @@ agrr_bid_1min = agrr_bid_1min.rename("aggressive ask price")
 print(agrr_bid_1min)
 
 
-# 5
+#5.VWAP of trades (and separately buys/sells) over interval
 def custom_agg_function(data):
     weighted_prices = (data["price"] * data["vol"]).sum()
     total_volume = data["vol"].sum()
 
     if total_volume == 0:
-        # Handle the case where total_volume is zero to avoid division by zero
         return 0.0
     else:
         return weighted_prices / total_volume
@@ -107,12 +105,12 @@ print(vwap_asks_1min)
 print(vwap_bids_1min)
 
 
-# 6
+#6. Simple average of trade prices (and separately buys/sells) over interval
 average_trades_1min = df.resample("1T", on="time").agg({"price": "mean"})
 average_asks_1min = Ask.resample("1T", on="time").agg({"price": "mean"})
 average_bids_1min = Bid.resample("1T", on="time").agg({"price": "mean"})
 
-# Rename the columns for clarity
+# Rename the columns 
 average_trades_1min = average_trades_1min.rename(columns={"price": "mean_price"})
 average_asks_1min = average_asks_1min.rename(columns={"price": "mean_price"})
 average_bids_1min = average_bids_1min.rename(columns={"price": "mean_price"})
@@ -121,8 +119,8 @@ print(average_trades_1min)
 print(average_asks_1min)
 print(average_bids_1min)
 
-# 7
-# Reset the indices for Ask and Bid DataFrames
+#7. Volume weighted average pre/post-trade bid/ask prices (measured just before each trade and weighted by the size of each trade)
+
 merged_df = pd.merge_asof(df, Ask, on='time', direction='backward', suffixes=('', '_ask'))
 merged_df = pd.merge_asof(merged_df, Bid, on='time', direction='backward', suffixes=('', '_bid'))
 
@@ -132,18 +130,16 @@ merged_df.set_index('time', inplace=True)
 merged_df.index = pd.to_datetime(merged_df.index)
 
 aggregation_rules = {
-    'price': 'mean',  # Taking the mean of the trade price
-    'vol': 'sum',     # Summing up the trade volumes
-    'weighted_bid': 'sum',  # Summing up the weighted bid values
-    'weighted_ask': 'sum',  # Summing up the weighted ask values
-    'price_bid': 'mean',    # Taking the mean of the bid prices
-    'price_ask': 'mean'     # Taking the mean of the ask prices
+    'price': 'mean',  
+    'vol': 'sum',   
+    'weighted_bid': 'sum',  
+    'weighted_ask': 'sum',  
+    'price_bid': 'mean',   
+    'price_ask': 'mean'     
 }
 
-# Resample and aggregate according to the defined rules
 one_minute_bins = merged_df.resample('1T').agg(aggregation_rules)
 
-# Calculate VWAP for each 1-minute bin
 one_minute_bins['vwap_bid'] = one_minute_bins['weighted_bid'] / one_minute_bins['vol']
 one_minute_bins['vwap_ask'] = one_minute_bins['weighted_ask'] / one_minute_bins['vol']
 print(one_minute_bins)
@@ -165,11 +161,46 @@ print(one_minute_bins_post)
 
 
 
-# 8
+#8. Time weighted version of bid/ask prices and size (in dollars) of best bid and ask (Holden and Jacobsen 2014)
+def collect_values(series):
+    return list(series.values) 
+Ask_resampled = Ask.resample('1T', on="time").agg({
+    'price': ['mean', collect_values],
+    'vol': ['mean', collect_values]  
+})
+Bid_resampled = Bid.resample('1T', on="time").agg({
+    'price': ['mean', collect_values],
+    'vol': ['mean', collect_values]
+})
 
+Ask_resampled.columns = ['price_mean', 'price_details', 'vol_sum', 'vol_details']
+Bid_resampled.columns = ['price_mean', 'price_details', 'vol_mean', 'vol_details']
 
+print(Ask_resampled)
+print(Bid_resampled)
 
-# 9
+def interpolate_timestamps(row):
+    I = len(row['price_details'])  
+    if I > 0:
+        i = np.arange(1, I + 1)
+        interpolated_times = row.name + pd.to_timedelta(((2 * i - 1) / (2 * I)), unit='s')
+        return interpolated_times
+    else:
+        return pd.Series()
+
+Ask_interpolated_times = Ask_resampled.apply(interpolate_timestamps, axis=1).explode().reset_index()
+Bid_interpolated_times = Bid_resampled.apply(interpolate_timestamps, axis=1).explode().reset_index()
+
+Ask_interpolated_times['price'] = Ask_resampled['price_details'].explode().values
+Ask_interpolated_times['vol'] = Ask_resampled['vol_details'].explode().values
+
+Bid_interpolated_times['price'] = Bid_resampled['price_details'].explode().values
+Bid_interpolated_times['vol'] = Bid_resampled['vol_details'].explode().values
+
+print(Ask_interpolated_times)
+print(Bid_interpolated_times)
+
+#9. Total Volume traded over interval
 df_resampled = df.resample("1T", on="time").agg({"vol": "sum"})
 print(df_resampled)
 Ask_resampled = Ask.resample("1T", on="time").agg({"vol": "sum"})
@@ -178,7 +209,7 @@ Bid_resampled = Bid.resample("1T", on="time").agg({"vol": "sum"})
 print(Bid_resampled)
 
 
-# 10
+#10. Number of bid/ask price/volume changes over interval
 def count_changes(series):
     price_changes = series.diff()
     num_changes = (price_changes != 0).sum()
