0dea3538cf02019510b8f8f2dbf295b74287a652
Konstantina Panagopoulou <151877044+konstantina44panag@users.noreply.github.com>
2024-01-27 20:19:50 +0200

    Update preparation.py

--- preparation.py
+++ preparation.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
 import pandas as pd
 import numpy as np
 import datetime
+import subprocess
 
+subprocess.run(["python3", "sign_algorithms.py"])
+from sign_algorithms import tradessigns
 
 TRADES_DATA_PATH = "/home/konstantina/ctm/trades_data.csv"
 QUOTES_DATA_PATH = "/home/konstantina/complete_nbbo/quotes_data.csv"
@@ -9,8 +13,16 @@ QUOTES_DATA_PATH = "/home/konstantina/complete_nbbo/quotes_data.csv"
 
 # Read and process trade data
 df = pd.read_csv(TRADES_DATA_PATH)
+buys_df = tradessigns[tradessigns['Initiator'] == 1]
+sells_df = tradessigns[tradessigns['Initiator'] == -1]
 df = df[["TIME_M", "PRICE", "SIZE"]].rename(
     columns={"TIME_M": "time", "PRICE": "price", "SIZE": "vol"}
+)
+buys_df=buys_df[["regular_timestamp", "price", "vol"]].rename(
+    columns={"regular_timestamp": "time"}
+)
+sells_df=sells_df[["regular_timestamp", "price", "vol"]].rename(
+    columns={"regular_timestamp": "time"}
 )
 # Read and process quote data
 df2 = pd.read_csv(QUOTES_DATA_PATH)
@@ -22,16 +34,27 @@ Bid = df2[["TIME_M", "BEST_BID", "Best_BidSizeShares"]].rename(
 )
 
 
+
 df["time"] = pd.to_datetime(df["time"], format="%H:%M:%S.%f", errors="coerce")
+buys_df["time"] = pd.to_datetime(buys_df["time"], format="%H:%M:%S.%f", errors="coerce")
+sells_df["time"] = pd.to_datetime(sells_df["time"], format="%H:%M:%S.%f", errors="coerce")
 Ask["time"] = pd.to_datetime(Ask["time"], format="%H:%M:%S.%f", errors="coerce")
 Bid["time"] = pd.to_datetime(Bid["time"], format="%H:%M:%S.%f", errors="coerce")
+
 
 start_time = pd.to_datetime("09:30:00", format="%H:%M:%S")
 end_time = pd.to_datetime("16:00:00", format="%H:%M:%S")
 
 df = df[(df["time"] >= start_time) & (df["time"] < end_time)]
+buys_df = buys_df[(buys_df["time"] >= start_time) & (buys_df["time"] < end_time)]
+sells_df = sells_df[(sells_df["time"] >= start_time) & (sells_df["time"] < end_time)]
 Ask = Ask[(Ask["time"] >= start_time) & (Ask["time"] < end_time)]
 Bid = Bid[(Bid["time"] >= start_time) & (Bid["time"] < end_time)]
+
+
 df = df.dropna(subset=["time"])
+buys_df = buys_df.dropna(subset=["time"])
+sells_df = sells_df.dropna(subset=["time"])
 Ask = Ask.dropna(subset=["time"])
 Bid=Bid.dropna(subset=["time"])
+
